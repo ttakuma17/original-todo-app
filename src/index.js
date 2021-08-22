e6b5d96d175e7e23737a7a7292994206f8f02edd
ttakuma17 <76837734+ttakuma17@users.noreply.github.com>
2021-08-22 16:48:41 +0900

    Done : making createListFunction リストの作成ごとの関数の作成はできたが、まだ冗長なコードが多いので改善余地あり

--- src/index.js
+++ src/index.js
@@ -3,227 +3,208 @@ import "./styles.css";
 const onClickAdd = () => {
   const addTodo = document.getElementById("add-todo").value;
   document.getElementById("add-todo").value = "";
-  //console.log(addTodo);// 入力値が表示されることを確認
+  // console.log(addTodo); // 入力値が表示されることを確認
+  createNewTodolist(addTodo);
+};
 
-  // todo listの中身を書きたい
-  // divを作成する→子要素の作成
+// delete関数定義ゾーン //
+const deleteFromTodoList = (listclass, target) => {
+  document.getElementById(listclass).removeChild(target);
+};
+
+// createTodolist関数 Todoタスクの新規追加の場合のコード
+const createNewTodolist = (text) => {
   const div = document.createElement("div");
   div.className = "list-row";
-  // liタグの作成
-  const li = document.createElement("li");
-  li.innerText = addTodo;
-  //console.log(li);//　入力値がliタグのテキストとして格納されていることを確認
 
-  //TodoListのリストに表示したい
-  div.appendChild(li);
-  document.getElementById("incomplete-list").appendChild(div);
-  //入力値が表示されたことを確認　→　それぞれのボタンを作成したい
+  //liタグ作成
+  const li = document.createElement("li");
+  li.innerText = text;
+
   const workingButton = document.createElement("button");
   workingButton.innerText = "Working";
-
-  //処理内容を考える
-  //Todo Listから入力値とボタンをを削除する
-  //addtodoの入力値を受け取って、workingtodoエリアで入力値とpendingbutton、Doneボタンを表示する
-
   workingButton.addEventListener("click", () => {
-    // workingボタンを押した時に削除される動作を実装したい
-    const deleteTarget = workingButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
-    // 処理確認→期待起動通り削除が完了
-    // working on Todoに表示させる
-    const addTarget = workingButton.parentNode;
-    const text = addTarget.firstElementChild.innerText;
-    // console.log(text);// 入力値が渡ったことを確認
-    // 同じ構成で表示できるようになればOKだが中身は微妙に違うので再指定する
-    addTarget.textContent = null;
-    // console.log(div);
-    const li = document.createElement("li");
-    li.innerText = text;
-    // console.log(li);
-    //ボタンの作成
-    const pendingButton = document.createElement("button");
-    pendingButton.innerText = "Pending";
-    // pendingボタンをクリックされたときの挙動を記載する
-    pendingButton.addEventListener("click", () => {
-      // alert("pending");
-      const deleteTarget = pendingButton.parentNode;
-      document.getElementById("working-list").removeChild(deleteTarget);
-      const text = pendingButton.parentNode.firstElementChild.innerText;
-      const div = document.createElement("div");
-      div.className = "list-row";
-      //liタグ作成
-      const li = document.createElement("li");
-      li.innerText = text;
-      // console.log(li);
-      // pending todo listに配置スべきボタンを作成して、要素を格納する
-      // 配置するボタンの定義
-      const backTodoButton = document.createElement("button");
-      backTodoButton.innerText = "Back to Todo";
-      //backTodoButtonがクリックされたときの動きを記載する
-      backTodoButton.addEventListener("click", () => {
-        // alert("back todo");//alertの動作確認
-        const deleteTarget = backTodoButton.parentNode;
-        document.getElementById("pending-list").removeChild(deleteTarget);
-        const text = backTodoButton.parentNode.firstElementChild.innerText;
-
-        const div = document.createElement("div");
-        div.className = "list-row";
-        //liタグ作成
-        const li = document.createElement("li");
-        li.innerText = text;
-        // console.log(text); //ここまでは稼働
-        //todoリストに必要な要素を追加
-        div.appendChild(li);
-        div.appendChild(workingButton);
-        div.appendChild(pendingButton);
-        div.appendChild(deleteButton);
-        // working-listへdiv要素をappend
-        // console.log(div); //div要素の追加までは完了
-        document.getElementById("incomplete-list").appendChild(div);
-      });
-
-      const backtoWorkButton = document.createElement("button");
-      backtoWorkButton.innerText = "Back to Work";
-      backtoWorkButton.addEventListener("click", () => {
-        //alert("back to work"); //稼働確認
-        const deleteTarget = backtoWorkButton.parentNode;
-        document.getElementById("pending-list").removeChild(deleteTarget);
-        const text = backtoWorkButton.parentNode.firstElementChild.innerText;
-
-        const div = document.createElement("div");
-        div.className = "list-row";
-        //liタグ作成
-        const li = document.createElement("li");
-        li.innerText = text;
-        // console.log(text); //ここまでは稼働
-        //todoリストに必要な要素を追加
-        div.appendChild(li);
-        div.appendChild(pendingButton);
-        div.appendChild(completeButton);
-        // console.log(div);//挙動確認
-        // working-listへdiv要素をappend
-        // console.log(div); //div要素の追加までは完了
-        document.getElementById("working-list").appendChild(div);
-      });
-
-      div.appendChild(li);
-      div.appendChild(backTodoButton);
-      div.appendChild(backtoWorkButton);
-      // pending-listに追加
-      document.getElementById("pending-list").appendChild(div);
-    });
-
-    // pendingの処理記入完了後に関数作成に取り掛かる　ほぼ同じになりそう
-    const completeButton = document.createElement("button");
-    completeButton.innerText = "Done";
-    completeButton.addEventListener("click", () => {
-      // alert("Done");
-      const deleteTarget = completeButton.parentNode;
-      document.getElementById("working-list").removeChild(deleteTarget);
-      const text = pendingButton.parentNode.firstElementChild.innerText;
-      const div = document.createElement("div");
-      div.className = "list-row";
-      //liタグ作成
-      const li = document.createElement("li");
-      li.innerText = text;
-      // console.log(li);
-      // pending todo listに配置スべきボタンを作成して、要素を格納する
-      // 配置するボタンの定義
-      const backTodoButton = document.createElement("button");
-      backTodoButton.innerText = "Back to Todo";
-      const deleteButton = document.createElement("button");
-      deleteButton.innerText = "Delete";
-      //backTodoButtonがクリックされたときの動きを記載する
-
-      backTodoButton.addEventListener("click", () => {
-        // alert("back todo"); //alertの動作確認
-        const deleteTarget = backTodoButton.parentNode;
-        document.getElementById("complete-list").removeChild(deleteTarget);
-        const text = backTodoButton.parentNode.firstElementChild.innerText;
-        const div = document.createElement("div");
-        div.className = "list-row";
-        //liタグ作成
-        const li = document.createElement("li");
-        li.innerText = text;
-        // console.log(text); //ここまでは稼働
-        //todoリストに必要な要素を追加
-        div.appendChild(li);
-        div.appendChild(workingButton);
-        div.appendChild(pendingButton);
-        div.appendChild(deleteButton);
-        // working-listへdiv要素をappend
-        // console.log(div); //div要素の追加までは完了
-        document.getElementById("incomplete-list").appendChild(div);
-      });
-      deleteButton.addEventListener("click", () => {
-        const deleteTarget = deleteButton.parentNode;
-        document.getElementById("complete-list").removeChild(deleteTarget);
-      });
-      div.appendChild(li);
-      div.appendChild(backTodoButton);
-      div.appendChild(deleteButton);
-      // completed Todo Listに追加
-      document.getElementById("complete-list").appendChild(div);
-    });
-
-    //divタグの子要素に各要素を設定
-    addTarget.appendChild(li);
-    addTarget.appendChild(pendingButton);
-    addTarget.appendChild(completeButton);
-    // console.log(addTarget); working　on todo の画面と同じ項目の追加に成功
-    document.getElementById("working-list").appendChild(addTarget);
-    // const completeButton = document.createElement("button");
-    // completeButton.innerText = "Done";
-  });
+    // alert("working"); //稼働確認
+    createWorkingList(workingButton.parentNode);
+  }); //引数の指定の仕方が悪かっただけだった
 
   const pendingButton = document.createElement("button");
   pendingButton.innerText = "Pending";
   pendingButton.addEventListener("click", () => {
-    // workingリストからの削除を実施
-    const deleteTarget = pendingButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
-    //入力値を受け取りたい
-    const addTarget = pendingButton.parentNode;
-    const text = addTarget.firstElementChild.innerText;
-    // console.log(text); // 入力値が渡ったことを確認
-    addTarget.textContent = null;
-    // console.log(div);
-    const li = document.createElement("li");
-    li.innerText = text;
-    // console.log(li);
-    //ボタンの作成
-    const backTodoButton = document.createElement("button");
-    backTodoButton.innerText = "Back to Todo";
-    const backtoWorkButton = document.createElement("button");
-    backtoWorkButton.innerText = "Back to Working";
-    //divタグの子要素に各要素を設定
-    addTarget.appendChild(li);
-    addTarget.appendChild(backTodoButton);
-    addTarget.appendChild(backtoWorkButton);
-    // console.log(addTarget); working　on todo の画面と同じ項目の追加に成功
-    document.getElementById("pending-list").appendChild(addTarget);
+    // alert("pending"); //稼働確認
+    // createPendingList(pendingButton);
+    createPendingList(pendingButton.parentNode, "incomplete-list");
   });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "Delete";
   deleteButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    // alert("delete"); //稼働確認
+    deleteFromTodoList("incomplete-list", deleteButton.parentNode);
   });
 
-  // const backTodoButton = document.createElement("button");
-  // backTodoButton.innerText = "Back to Todo";
-  // backTodoButton.addEventListener("click", () => {});
-
-  // const backtoWorkButton = document.createElement("button");
-  // backtoWorkButton.innerText = "Back to Work";
-  // backtoWorkButton.addEventListener("click", () => {});
-
+  //todoリストに必要な要素を追加
   div.appendChild(li);
   div.appendChild(workingButton);
   div.appendChild(pendingButton);
   div.appendChild(deleteButton);
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+// // createWorkingList関数
+const createWorkingList = (target, listclass) => {
+  if (listclass) {
+    // incomplete-listだったら実行しないの条件式にかえる
+    const deleteTarget = target;
+    // listclassの値によって削除するかどうかを決定する必要がある。
+    document.getElementById(listclass).removeChild(deleteTarget);
+  }
+  //第二引数は、削除するリストクラスを指定するものになる
+  // 下の２文はPending Listのボタンを押された時に必要になるがcreateTodoから呼び出されたときは不要
+  // const deleteTarget = target;
+  // listclassの値によって削除するかどうかを決定する必要がある。
+  // document.getElementById(listclass).removeChild(deleteTarget);
+  const addTarget = target;
+  // console.log(addTarget); //稼働確認
+  const text = addTarget.firstElementChild.innerText;
+  // console.log(text);
+  addTarget.textContent = null;
+  const li = document.createElement("li");
+  li.innerText = text;
+  // console.log(li); //稼働確認
+
+  const pendingButton = document.createElement("button");
+  pendingButton.innerText = "Pending";
+  pendingButton.addEventListener("click", () => {
+    //alert("pending"); //稼働確認
+    createPendingList(pendingButton.parentNode, "working-list");
+  });
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "Done";
+  completeButton.addEventListener("click", () => {
+    // alert("done");
+    createCompleteTodoList(pendingButton.parentNode, "working-list");
+  });
+
+  addTarget.appendChild(li);
+  addTarget.appendChild(pendingButton);
+  addTarget.appendChild(completeButton);
+  document.getElementById("working-list").appendChild(addTarget);
+};
+
+// // createPendingList関数
+const createPendingList = (target, listclass) => {
+  const deleteTarget = target; //ID取得の引数を指定することが必要
+  document.getElementById(listclass).removeChild(deleteTarget);
+  //入力値を受け取りたい
+  const addTarget = target;
+  const text = addTarget.firstElementChild.innerText;
+  // console.log(text); // 入力値が渡ったことを確認
+  addTarget.textContent = null;
   // console.log(div);
+  const li = document.createElement("li");
+  li.innerText = text;
+
+  //ボタンの作成
+  const backTodoButton = document.createElement("button");
+  backTodoButton.innerText = "Back to Todo";
+  backTodoButton.addEventListener("click", () => {
+    // alert("test");
+    createBackTodolist(backTodoButton.parentNode, "pending-list");
+    // return ;//textを返すように指定する?
+    //createTodolist(text);
+  });
+
+  const workingButton = document.createElement("button");
+  workingButton.innerText = "Working";
+  workingButton.addEventListener("click", () => {
+    // alert("working"); //稼働確認
+    createWorkingList(workingButton.parentNode, "pending-list");
+  });
+  //divタグの子要素に各要素を設定
+  addTarget.appendChild(li);
+  addTarget.appendChild(backTodoButton);
+  addTarget.appendChild(workingButton);
+  // console.log(addTarget); working　on todo の画面と同じ項目の追加に成功
+  document.getElementById("pending-list").appendChild(addTarget);
+};
+
+const createCompleteTodoList = (target, listclass) => {
+  // alert("back to do");
+  const deleteTarget = target; //ID取得の引数を指定することが必要
+  document.getElementById(listclass).removeChild(deleteTarget);
+  //入力値を受け取りたい
+  const addTarget = target;
+  const text = addTarget.firstElementChild.innerText;
+  // console.log(text); // 入力値が渡ったことを確認
+  addTarget.textContent = null;
+  // console.log(div);
+  const li = document.createElement("li");
+  li.innerText = text;
+
+  const backTodoButton = document.createElement("button");
+  backTodoButton.innerText = "Back to Todo";
+  backTodoButton.addEventListener("click", () => {
+    // alert("test");
+    createBackTodolist(backTodoButton.parentNode, "complete-list");
+    // return ;//textを返すように指定する?
+    //createTodolist(text);
+  });
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", () => {
+    // alert("delete"); //稼働確認
+    deleteFromTodoList("complete-list", deleteButton.parentNode);
+  });
+  addTarget.appendChild(li);
+  // addTarget.appendChild(workingButton);
+  // addTarget.appendChild(pendingButton);
+  addTarget.appendChild(backTodoButton);
+  addTarget.appendChild(deleteButton);
+  // console.log(addTarget); working　on todo の画面と同じ項目の追加に成功
+  document.getElementById("complete-list").appendChild(addTarget);
+};
+
+const createBackTodolist = (target, listclass) => {
+  const deleteTarget = target; //ID取得の引数を指定することが必要
+  document.getElementById(listclass).removeChild(deleteTarget);
+  //入力値を受け取りたい
+  const addTarget = target;
+  const text = addTarget.firstElementChild.innerText;
+  // console.log(text); // 入力値が渡ったことを確認
+  addTarget.textContent = null;
+  // console.log(div);
+  const li = document.createElement("li");
+  li.innerText = text;
+  const workingButton = document.createElement("button");
+  workingButton.innerText = "Working";
+  workingButton.addEventListener("click", () => {
+    // alert("working"); //稼働確認
+    createWorkingList(workingButton.parentNode);
+  }); //引数の指定の仕方が悪かっただけだった
+
+  const pendingButton = document.createElement("button");
+  pendingButton.innerText = "Pending";
+  pendingButton.addEventListener("click", () => {
+    // alert("pending"); //稼働確認
+    // createPendingList(pendingButton);
+    createPendingList(pendingButton.parentNode, "incomplete-list");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", () => {
+    // alert("delete"); //稼働確認
+    deleteFromTodoList("incomplete-list", deleteButton.parentNode);
+  });
+
+  //todoリストに必要な要素を追加
+  addTarget.appendChild(li);
+  addTarget.appendChild(workingButton);
+  addTarget.appendChild(pendingButton);
+  addTarget.appendChild(deleteButton);
+  document.getElementById("incomplete-list").appendChild(addTarget);
 };
 
 document
